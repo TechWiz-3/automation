ef612518f9e91ac1120cbf3cd689424e9005a6ec
TechWiz-3 <programmingplus1@gmail.com>
2022-04-25 20:14:01 +1000

    add proper response for no meetings on

--- meetings/main.py
+++ meetings/main.py
@@ -1,3 +1,8 @@
+# Simple automated script that provides me
+# with the MS Teams link for my classes
+# based on the date and time
+# Created by Zac the Wise on 25/April/2022
+
 import datetime
 from dotenv import load_dotenv
 from os import getenv
@@ -8,10 +13,10 @@ pretty = Console()
 
 load_dotenv()
 MON_MORN = getenv("MON_MORN")
-MON_AFT = getenv("MON_MORN")
-TUES_MORN = getenv("MON_MORN")
-TUES_AFT = getenv("MON_MORN")
-THUR_ALLD = getenv("MON_MORN")
+MON_AFT = getenv("MON_AFT")
+TUES_MORN = getenv("TUES_MORN")
+TUES_AFT = getenv("TUES_AFT")
+THUR_ALLD = getenv("THUR_ALLD")
 
 def what_day():
     """Returns the weekday"""
@@ -30,6 +35,7 @@ def what_day():
     return "sun"
 
 def what_time():
+    """Returns if the current time is after midday or not"""
     current_date = datetime.datetime.now()
     return (current_date.time() < datetime.time(12))
 
@@ -57,7 +63,7 @@ def get_link():
 def main():
     link = get_link()
     if link == False:
-        pretty.print("helloooooo", style = "green")
+        pretty.print("No classes today, now that's actually pog", style = "green")
     else:
         pretty.print("You have a class now, that's so pog :sweat_smile: :sob:", style = "bold red")
         pretty.print(link, style = "blue underline")
