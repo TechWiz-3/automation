144412ef2085f346010f852f6216e334613e4e90
TechWiz-3 <programmingplus1@gmail.com>
2022-05-11 21:59:33 +1000

    functional but super primitive player (im already using and enjoying oit)

--- playlist-player/python3/playlist_play.py
+++ playlist-player/python3/playlist_play.py
@@ -1,13 +1,15 @@
-#  figure out how turning a python script
-# into a full on bash command works with
-# switches and such, using argv?
+# Created by Zac the Wise
+# A tool that can play music from a local folder in a playlist sort of way
+# no more spotify or itunes
+# In the future I will add tools that will allow the construction of said
+# playlists using yt and spotify download tools
 
-from os import chdir
-from os import system
 import glob
+from playsound import playsound
 
-folder = input("Enter folder of playlist from root")
-files = glob.glob(f"{folder}/*")
-for file in files:
-    print(file)
-    system("afplay file")
+# default playlist folder for the time being
+playlist_folder = '/Users/Peregrine/Desktop/Other Music/'
+folder_items = glob.glob(f"{playlist_folder}*.mp*")  # get mp3 and mp4
+for item in folder_items:
+    print(f"Now playing, {item}")
+    playsound(item)
